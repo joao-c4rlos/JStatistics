9e17399d55d86d755f547b47936a02be4cc84895
ead <ead@academico.local>
2025-04-28 12:05:22 -0300

    aula

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alo git!");
+        System.out.println("\t\tJEstatistics");
+        System.out.print("-- Controle de Dados Empresarial -- ");
+        System.out.print("2025");
+        //ArrayList lista = new ArrayList();
+        
     }
 }
