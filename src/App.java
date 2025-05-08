aaecc6b2e6aa1193524ea3f694983acd0f159975
ead <ead@academico.local>
2025-05-08 11:39:36 -0300

    aula 6 bem executada e produtiva

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alo git!");
+        // Classe é um conjunto de objetos
+        // Nosso objetivo nesse código é entender a diferença
+        // Entre método com o tipo void e método com tipo String
+        Mamifero m1 = new Mamifero();
+        m1.setNomeCientifico("Bos Taurus");
+        Mamifero m2 = new Mamifero();
+        m2.setNomeCientifico("Macropus");
+        Mamifero m3 = new Mamifero();
+        m3.setNomeCientifico("Trichechus");
+
+        JOptionPane.showMessageDialog(null, "Nome cientifico:" + m1.getNomeCientifico(), "linhagem dos mamíferos",
+                JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Nome cientifico:" + m2.getNomeCientifico(), "linhagem dos mamíferos",
+                JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Nome cientifico:" + m3.getNomeCientifico(), "linhagem dos mamíferos",
+                JOptionPane.INFORMATION_MESSAGE);
+
     }
 }
---
+++ src/Mamifero.java
@@ -0,0 +1,17 @@
+public class Mamifero {
+
+    private int id;
+    private String nomeCientifico;
+    
+    public int getId() {
+        return this.id;
+    }
+    public String getNomeCientifico() {
+        return this.nomeCientifico;
+    }
+    public void setNomeCientifico(String nomeCientifico) {
+        this.nomeCientifico = nomeCientifico;
+    }
+
+    
+}
