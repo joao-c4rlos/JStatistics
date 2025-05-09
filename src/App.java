25ed324bc1d2795f376f9d7110189c42ed780fa4
ead <ead@academico.local>
2025-05-09 09:25:52 -0300

    ex1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import javax.swing.JOptionPane;
+
 public class App {
         public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Alô, Jardim!", null, JOptionPane.INFORMATION_MESSAGE);
 
         }
 }
