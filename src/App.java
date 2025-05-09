05f382f9f908256a8060d676b47f0cac33f8b3ee
ead <ead@academico.local>
2025-05-09 09:28:49 -0300

    ex2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import javax.swing.JOptionPane;
+
 public class App {
         public static void main(String[] args) throws Exception {
-
+                String nome = JOptionPane.showInputDialog(null, "Digite o seu nome:", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+        String endereço = JOptionPane.showInputDialog(null, "Digite o seu endereço:", "Cadastro", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, nome  + endereço, "seu cadastro", JOptionPane.INFORMATION_MESSAGE);
         }
 }
