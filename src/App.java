a65ae158beaf5583b71c297b9a17ab05e06e38f8
ead <ead@academico.local>
2025-05-13 08:53:52 -0300

    ex14

--- src/App.java
+++ src/App.java
@@ -2,12 +2,14 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Faça o seu cadastro", "",JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Faça o seu cadastro", "", JOptionPane.INFORMATION_MESSAGE);
         String nome = JOptionPane.showInputDialog("Insira o seu nome:");
         String endereco = JOptionPane.showInputDialog("Insira o seu endereço:");
-        double telefone = Double.parseDouble(JOptionPane.showInputDialog("Digite o seu telefone:"));
+        String telefone = JOptionPane.showInputDialog("Digite o seu telefone:");
 
-        JOptionPane.showMessageDialog(null, "Esses são os seus dados cadastrados"+ nome +"\n"+ endereco + "\n" + telefone, "Dados cadastrados", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,
+                "Esses são os seus dados cadastrados:" + "\n" + nome + "\n" + endereco + "\n" + telefone,
+                "Dados cadastrados", JOptionPane.INFORMATION_MESSAGE);
 
-}
+    }
 }
